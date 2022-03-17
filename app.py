bf8783d7eccd0c716651de6c837a412f34e7af7a
Weston Shuken <westonshuken@gmail.com>
2022-03-17 14:38:46 -0400

    cleaned index md, added sidebar to app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import textinput
 import streamlit as st
 import pickle
 import pandas as pd
@@ -33,6 +34,40 @@ else:
     st.markdown('##### *i am waiting......*')
     st.image('images/shiba_gif_200.gif')
 
-# st.sidebar.header('We are VIA GoGo')
+st.sidebar.markdown("# Brought to you by")
+
+st.sidebar.image("images/ViaGoGo_logo.png", width=175)
+
+st.sidebar.markdown("## Background") 
+st.sidebar.markdown("Understanding brand and product reputation \
+    is difficult when companies only have access to customer surveys and online review data. \
+    However, there is an abundance of social media user posts about products and brands from various platforms. \
+    With these unofficial reviews and preferences towards products via tweets from **Twitter**, \
+    we can derive an overall sentiment towards your brand and products. \
+    Our company, ViaGoGo, can provide you with a state-of-the-art machine learning model that rates your products \
+    and brand sentiment based tweet towards your company. Test it out by typing a tweet on the right. ---> ") 
+
+
+#### WORKING ON ADDING AN API PULL OF TWEETS ####
+
+# st.sidebar.markdown("You can also type in your company's Twitter handle below and collexct a \
+#     sample of today's tweets with their sentiment scores.")
+
+# handle = st.sidebar.text_input('Twitter Handle', value='@Google')
+# generate = st.sidebar.button('Collect Samples')
+
+st.sidebar.markdown("## Data & Methods") 
+
+st.sidebar.markdown("The model was trained on a dataset from Crowdflower via [data.world](https://data.world/crowdflower/brands-and-product-emotions) *Created: August 30, 2013 by Kent Cavender-Bares*")
+st.sidebar.markdown("The data contains over 9,000 tweets from Twitter users that evaluated multiple brands and products. \
+    The crowd was asked if the tweet expressed positive, negative, or no emotion towards a brand and/or product. \
+    If some emotion was expressed, they were also asked to say which brand or product was the target of that emotion.") 
+st.sidebar.markdown("#### For more information")
+
+st.sidebar.markdown("Please check out our [Github](https://github.com/westonshuken/Twitter-Sentiment-Analysis)")
+
+#### WILL GENERATE SENTIMENT REPORT FROM API PULL ####
+# if generate:
+
 
 
